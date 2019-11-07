d16edb68f910af3a6fc969b911e2d32c060ec361
Rastafouille <jseyssaud@yahoo.fr>
2019-11-07 22:45:00 +0100

    arduino maj

--- ServoCommand/ServoCommand.ino
+++ ServoCommand/ServoCommand.ino
@@ -1,45 +1,56 @@
+// Servo  initialiser a 90 et apres +- 25° de débatement
+// marche arriere 25 max a 70 min
+// marche avant  150 max a 115 min
+
 #include <Servo.h>
 #include <Wire.h>
 
 Servo myservo;  // create servo object to control a servo
-
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+Servo mymotor;
 
 // Buffer qui va contenir la trame série
 #define TAILLE_MAX 32
 // Buffer qui va contenir le texte (taille du buffer / 2, oui j'ai mis ça au pif)
 char texte[TAILLE_MAX];
 // Données utiles extraites
-int cons_vitesse,cons_angle,angle;
-float vitesse;
+int cons_vitesse; //entre -100 et 100
+int cons_angle; // entre -25 et 25
+int vitesse=0;
+int angle=0;
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  mymotor.attach(6);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
+  mymotor.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(90);
   Serial.begin(9600);
-  Serial.println("Serial");
-  Wire.begin();
+  Serial.println("Serial OK");
+
+  //attente init controleur
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(5000);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);  // waits for the servo to get there
-
-
-
   // Récupération d'une trame + parsing
   if(recupInfo(texte,&cons_vitesse,&cons_angle)==1) {Serial.println("Erreur de trame 1!");}
   if(recupInfo(texte,&cons_vitesse,&cons_angle)==2) {Serial.println("Erreur de trame 2!");}  
   else {
-  cons_vitesse = map(cons_vitesse, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  mymotor.write(cons_vitesse);                  // sets the servo position according to the scaled value
-  cons_angle = map(cons_angle, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(cons_angle);                  // sets the servo position according to the scaled value
-  
-  }
+      if (cons_vitesse==0) {vitesse=0;}
+      else if (cons_vitesse<0){vitesse = int(70+ cons_vitesse*40/100);}
+      else if (cons_vitesse>0){vitesse = int (110+cons_vitesse*30/100);}
+      
+      mymotor.write(vitesse); 
+      Serial.print("Speed = "); Serial.println(vitesse);
+      if (cons_angle>25) {angle=25;}
+      else if (cons_angle<-25) {angle=-25;}
+      else {angle=cons_angle;} 
+      myservo.write(90+angle);    
+      Serial.print("teta = "); Serial.println(angle);             
+       }
+  delay(20);
 
 }
 
@@ -62,8 +73,11 @@ int recupInfo(char *texte, int *cons_vitesse,int *cons_angle) {
       /* Parse la chaine de caractères et extrait les champs */
       Serial.println(texte);
       if(sscanf(texte, "X;%d;%d;",cons_vitesse,cons_angle) != 2)
+      
       {//Serial.print("test");Serial.print(*cons_vitesse);Serial.println(*cons_angle);
         return 2;} /* Si sscanf n'as pas pu extraire les 2 champs -> erreur*/
+        Serial.print("cons teta = "); Serial.println(*cons_angle);
+      Serial.print("cons Speed = "); Serial.println(*cons_vitesse);
   return 0;/* retourne 0 -> pas d'erreur */
 }
 }
